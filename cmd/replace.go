c585c32dca5a56fe48e6b77a7e7c6be388dba1b6
garrou <garrousteadrien@gmail.com>
2023-01-24 11:32:12 +0100

    Add replace cmd

--- cmd/replace.go
+++ cmd/replace.go
@@ -1,1 +1,28 @@
 package cmd
+
+import (
+	"lazy/lib"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	replaceCmd = &cobra.Command{
+		Use:   "replace [name] [path]",
+		Short: "Replace a lazy named [name] with [path]",
+		Long:  "Replace a lazy named [name] with the new path [path]",
+		Run: func(cmd *cobra.Command, args []string) {
+			name, path := lib.BindNamePath(args)
+			err := lib.ReplaceLazy(name, path)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(replaceCmd)
+}
